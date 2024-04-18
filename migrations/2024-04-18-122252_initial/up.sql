d3e88d0cc8be5323cd7b19a6d665832a09ec25fe
Eugene Michkov <michkov.evgeny@gmail.com>
2024-04-19 00:23:31 +0300

    changed case

--- migrations/2024-04-18-122252_initial/up.sql
+++ migrations/2024-04-18-122252_initial/up.sql
@@ -25,24 +25,24 @@ CREATE TYPE "UserStatus" AS ENUM (
 CREATE TABLE
     "Server" (
         "id" TEXT NOT NULL,
-        "publicKey" TEXT NOT NULL,
-        "backendUri" TEXT NOT NULL,
-        "wireguardUri" TEXT NOT NULL,
+        "public_key" TEXT NOT NULL,
+        "backend_uri" TEXT NOT NULL,
+        "wireguard_uri" TEXT NOT NULL,
         "country" "Country" NOT NULL,
-        "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        "updatedAt" TIMESTAMP(3) NOT NULL,
+        "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "updated_at" TIMESTAMP(3) NOT NULL,
         CONSTRAINT "Server_pkey" PRIMARY KEY ("id")
     );
 
 CREATE TABLE
     "Config" (
         "id" TEXT NOT NULL,
-        "privateKey" TEXT NOT NULL,
-        "userIp" TEXT NOT NULL,
-        "serverId" TEXT NOT NULL,
+        "private_key" TEXT NOT NULL,
+        "user_ip" TEXT NOT NULL,
+        "server_id" TEXT NOT NULL,
         "status" "ConfigStatus" NOT NULL DEFAULT 'NotInUse',
-        "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        "updatedAt" TIMESTAMP(3) NOT NULL,
+        "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "updated_at" TIMESTAMP(3) NOT NULL,
         CONSTRAINT "Config_pkey" PRIMARY KEY ("id")
     );
 
@@ -51,11 +51,11 @@ CREATE TABLE
         "id" TEXT NOT NULL,
         "email" TEXT NOT NULL,
         "password" TEXT NOT NULL,
-        "bannedAt" TIMESTAMP(3),
-        "bannedTill" TIMESTAMP(3),
+        "banned_at" TIMESTAMP(3),
+        "banned_till" TIMESTAMP(3),
         "status" "UserStatus" NOT NULL DEFAULT 'Active',
-        "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        "updatedAt" TIMESTAMP(3) NOT NULL,
+        "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "updated_at" TIMESTAMP(3) NOT NULL,
         CONSTRAINT "User_pkey" PRIMARY KEY ("id")
     );
 
@@ -64,13 +64,13 @@ CREATE TABLE
         "id" TEXT NOT NULL,
         "name" TEXT NOT NULL,
         "os" "OS" NOT NULL,
-        "userId" TEXT NOT NULL,
-        "bannedAt" TIMESTAMP(3),
-        "bannedTill" TIMESTAMP(3),
-        "revokedAt" TIMESTAMP(3),
+        "user_id" TEXT NOT NULL,
+        "banned_at" TIMESTAMP(3),
+        "banned_till" TIMESTAMP(3),
+        "revoked_at" TIMESTAMP(3),
         "status" "DeviceStatus" NOT NULL DEFAULT 'LoggedIn',
-        "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        "updatedAt" TIMESTAMP(3) NOT NULL,
+        "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "updated_at" TIMESTAMP(3) NOT NULL,
         CONSTRAINT "Device_pkey" PRIMARY KEY ("id")
     );
 
@@ -78,31 +78,31 @@ CREATE TABLE
     "Session" (
         "id" TEXT NOT NULL,
         "status" "SessionStatus" NOT NULL DEFAULT 'Active',
-        "openedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        "closedAt" TIMESTAMP(3),
-        "deviceId" TEXT NOT NULL,
-        "configId" TEXT NOT NULL,
+        "opened_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "closed_at" TIMESTAMP(3),
+        "device_id" TEXT NOT NULL,
+        "config_id" TEXT NOT NULL,
         CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
     );
 
-CREATE UNIQUE INDEX "Server_publicKey_key" ON "Server" ("publicKey");
+CREATE UNIQUE INDEX "Server_publicKey_key" ON "Server" ("public_key");
 
-CREATE UNIQUE INDEX "Server_backendUri_key" ON "Server" ("backendUri");
+CREATE UNIQUE INDEX "Server_backendUri_key" ON "Server" ("backend_uri");
 
-CREATE UNIQUE INDEX "Server_wireguardUri_key" ON "Server" ("wireguardUri");
+CREATE UNIQUE INDEX "Server_wireguardUri_key" ON "Server" ("wireguard_uri");
 
-CREATE UNIQUE INDEX "Config_privateKey_key" ON "Config" ("privateKey");
+CREATE UNIQUE INDEX "Config_privateKey_key" ON "Config" ("private_key");
 
 CREATE UNIQUE INDEX "User_email_key" ON "User" ("email");
 
-CREATE UNIQUE INDEX "Session_deviceId_key" ON "Session" ("deviceId");
+CREATE UNIQUE INDEX "Session_deviceId_key" ON "Session" ("device_id");
 
-CREATE UNIQUE INDEX "Session_configId_key" ON "Session" ("configId");
+CREATE UNIQUE INDEX "Session_configId_key" ON "Session" ("config_id");
 
 ALTER TABLE "Config" ADD CONSTRAINT "Config_serverId_fkey" FOREIGN KEY ("serverId") REFERENCES "Server" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
-ALTER TABLE "Device" ADD CONSTRAINT "Device_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Device" ADD CONSTRAINT "Device_userId_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
-ALTER TABLE "Session" ADD CONSTRAINT "Session_deviceId_fkey" FOREIGN KEY ("deviceId") REFERENCES "Device" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Session" ADD CONSTRAINT "Session_deviceId_fkey" FOREIGN KEY ("device_id") REFERENCES "Device" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
-ALTER TABLE "Session" ADD CONSTRAINT "Session_configId_fkey" FOREIGN KEY ("configId") REFERENCES "Config" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Session" ADD CONSTRAINT "Session_configId_fkey" FOREIGN KEY ("config_id") REFERENCES "Config" ("id") ON DELETE RESTRICT ON UPDATE CASCADE;
